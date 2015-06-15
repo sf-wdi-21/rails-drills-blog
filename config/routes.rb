aa463551716d5ac40b86d4dfa381a16350211d63
rycwilson <rycwilson@gmail.com>
2015-06-15 13:03:01 -0700

    index of articles on users#show

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new', as: 'signup'
   get '/login', to: 'sessions#new', as: 'login'
   post '/sessions', to: 'sessions#create'
-  get '/logout', to: 'sessions#destroy', as: 'logout'
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
 
   get '/users', to: 'users#index'
   post '/users', to: 'users#create'
