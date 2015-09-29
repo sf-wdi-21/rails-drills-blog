ff30e2f8190f976a9a5f4fda500f20288cec7a0a
rycwilson <rycwilson@gmail.com>
2015-09-29 09:50:42 -0700

    added missing avatar image

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
   # paperclip
   has_attached_file :avatar,
             styles: { medium: "300x300>", thumb: "100x100>" },
-       default_url: "/images/:style/missing.png"
+       default_url: ':style/missing-avatar.jpg'
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
   # returns @user or false
