4e2a7eb040810ad6f1b8155496596e0fee60452b
rycwilson <rycwilson@gmail.com>
2015-09-29 13:06:53 -0700

    user authorization bug fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,7 +64,8 @@ class UsersController < ApplicationController
   def auth_user?
     # layout: false prevents the application layout page from loading
     # returning false/true stops/allows the action
-    unless current_user.id == params[:id]
+    binding.pry
+    unless current_user.id == params[:id].to_i
       render file: 'public/401.html', status: 401, layout: false
       false
     else
